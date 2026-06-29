f09391a3090d84ec38d9333f4e8cc9a7b55552c3
dashnog <dashnog8@gmail.com>
2026-06-29 13:07:27 +1000

    Reorganized file structure, added music, and volume slider

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var game_scene_path = "res://world.tscn"
+@export var game_scene_path = "res://scenes/world.tscn"
 
 @onready var start_button = $"Background/CenterContainer/VBoxContainer/StartButton"
 @onready var quit_button = $"Background/CenterContainer/VBoxContainer/QuitButton"
@@ -11,11 +11,17 @@ func _ready() -> void:
 	
 	start_button.pressed.connect(start_game)
 	quit_button.pressed.connect(quit_game)
+	
+	$Background/CenterContainer/VBoxContainer/MusicVolumeSlider.set_value_no_signal(MusicManager.music_volume)
 
 
 func start_game() -> void:
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 
 func quit_game() -> void:
 	get_tree().quit()
+
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	MusicManager.set_volume(value)
